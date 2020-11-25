79b71f3bf5fb54501067caa2e974a0f804954415
Shafin580 <johndoe@example.com>
2020-11-25 22:26:28 +0600

    user list

--- FINAL_LAB_TASK_1.1_DB/controller/databaseConnect.php
+++ FINAL_LAB_TASK_1.1_DB/controller/databaseConnect.php
@@ -34,4 +34,37 @@ $result = mysqli_query($conn, $sql);
     }
  }
 
+function showAll($conn){
+    
+    $sql = "select * from users";
+    $result = mysqli_query($conn, $sql);
+    echo "<table border=1>
+    <tr>
+    <th>ID</th>
+    <th>USERNAME</th>
+    <th>PASSWORD</th>
+    <th>EMAIL</th>
+    <th>TYPE</th>
+    <th colspan=2>Action</th>
+    </tr>";
+    
+    while($row = mysqli_fetch_assoc($result)){
+        
+        echo "<tr>
+        <td>".$row['id']."</td>
+        <td>".$row['username']."</td>
+        <td>".$row['password']."</td>
+        <td>".$row['email']."</td>
+        <td>".$row['type']."</td>
+        <td><a href=updateUsers.php?msg=".$row['id'].">Update</a></td>
+        <td><a href=../controller/deleteUsers.php?msg=".$row['id'].">Delete</a></td>
+        </tr>";
+        
+    }
+    
+    echo "</table>";
+    
+    
+}
+
 ?>
