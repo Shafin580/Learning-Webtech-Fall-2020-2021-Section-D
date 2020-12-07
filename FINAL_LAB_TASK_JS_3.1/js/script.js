3f0b8f3d9180712bb44fbedc507791d10ff36066
Shafin580 <johndoe@example.com>
2020-12-07 10:05:45 +0600

    Gender Validation

--- FINAL_LAB_TASK_JS_3.1/js/script.js
+++ FINAL_LAB_TASK_JS_3.1/js/script.js
@@ -65,3 +65,19 @@ function emailValidation(){
     }
     
 }
+
+function genderValidation(){
+    
+    if(document.getElementById("maleId").checked|| document.getElementById("femaleId").checked|| document.getElementById("othersId").checked){
+        
+        alert("Gender Selected");
+        
+    }
+    
+    else{
+        
+        alert("Please Select Gender");
+        
+    }
+    
+}
