6ed3a3dbbfdd4be5d4eb926b10044b6725a8fac2
Shafin580 <johndoe@example.com>
2020-12-07 09:45:23 +0600

    Name validation

---
+++ FINAL_LAB_TASK_JS_3.1/js/script.js
@@ -0,0 +1,54 @@
+"use strict"
+
+
+
+function nameValidation() {
+    
+    var data = document.getElementById('nameId').value;
+    
+    if (data === "") {
+        
+        window.alert('Name is empty!');
+        
+    }
+    
+    else {
+        
+        let name_word = data.split(" ");
+        
+        if (name_word.length < 2) {
+            
+            window.alert('Name has less than two words!');
+            
+        }
+        
+        else {
+            
+            let pattern = ['<', ',', '>', '/', '?', '"', "'", ';', ':', ']', '[', '|', '}', '{', '=', '+',
+                            '_', ')', '(', '*', '&', '^', '%', '$', '#', '@', '!', '`', '~', '0', '1', '2', '3', 
+                            '4', '5', '6', '7', '8', '9'];
+            
+            let i;
+            for (i = 0; i < pattern.length; i++) {
+                
+                if (data.includes(pattern[i])) {
+                    
+                    window.alert("Invalid Name!");
+                    break;
+                    
+                }
+                
+                else if (data[0] == pattern[i]) {
+                    
+                    window.alert("Invalid Name!");
+                    break;
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
